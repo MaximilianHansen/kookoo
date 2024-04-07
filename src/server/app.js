209952204f786343e71d43a8d902a1042691d269
Maximilian Hansen <max.blade.hansen@gmail.com>
2024-04-07 15:22:09 -0400

    first commit

--- src/server/app.js
+++ src/server/app.js
@@ -30,7 +30,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use((req,res, next)=> {console.log(req.session); next()}
+app.use((req,res, next)=> {console.log(req.session); console.log(req.user); next()}
 
 )
 
