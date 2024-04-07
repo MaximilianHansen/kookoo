d437596ce1a8d6872e3f8fed8ff6c5cd7aefbb59
Ubuntu <ubuntu@ip-172-26-13-229.ec2.internal>
2024-04-07 18:33:35 +0000

    commit

---
+++ src/server/app.js
@@ -0,0 +1,63 @@
+var GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
+var express = require('express');
+var passport = require('passport');
+var app = express()
+const port = 3001
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  googleId: String,
+});
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+const User = mongoose.model('User', userSchema);
+
+app.get('/', (req,res)=>{
+    res.send('hello world!')
+})
+
+var GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
+
+passport.use(new GoogleStrategy({
+    clientID:     '1049417195222-5sra3k5h6muam47ufdkjsrg4v5uil6sg.apps.googleusercontent.com',
+    clientSecret: "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",
+    callbackURL: "http://maxhansen.co:3001/auth/google/callback",
+    passReqToCallback   : true
+  },
+  async function(request, accessToken, refreshToken, profile, done) {
+    try {
+      let user = await User.findOne({ googleId: profile.id });
+      if (!user) {
+        user = await User.create({ googleId: profile.id });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  }
+));
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope:
+      [ 'email', 'profile' ] }
+));
+
+app.get( '/auth/google/callback',
+    passport.authenticate( 'google', {
+        successRedirect: '/auth/google/success',
+        failureRedirect: '/auth/google/failure'
+}));
+
+app.get('/auth/google/success', (req, res) => {
+  res.send("logged in", req.body)
+})
+
+
+app.listen(port, ()=> {console.log(`running on ${port}`)})
